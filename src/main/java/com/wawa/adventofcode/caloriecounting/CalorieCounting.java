bf060eeee50203218f5e9bf3822bbf9b7fcf7959
Wawa27 <Oualid.hassan.pro@gmail.com>
2022-12-08 14:20:34 +0100

    first commit

---
+++ src/main/java/com/wawa/adventofcode/caloriecounting/CalorieCounting.java
@@ -0,0 +1,10 @@
+package com.wawa.adventofcode.caloriecounting;
+
+/**
+ * Hello world!
+ */
+public class CalorieCounting {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/wawa/adventofcode/CalorieCountingTest.java
@@ -0,0 +1,33 @@
+package com.wawa.adventofcode;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalorieCountingTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CalorieCountingTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(CalorieCountingTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
